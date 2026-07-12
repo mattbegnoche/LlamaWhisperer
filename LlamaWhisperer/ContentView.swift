2cab50f6c4a582c6c0b8f16e3604a29b65387f10
Matthew Begnoche <mattbegnochedev@gmail.com>
2026-07-12 10:36:27 -0500

    Initial Commit

---
+++ LlamaWhisperer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LlamaWhisperer
+//
+//  Created by Matt Begnoche on 7/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LlamaWhisperer/LlamaWhispererApp.swift
@@ -0,0 +1,17 @@
+//
+//  LlamaWhispererApp.swift
+//  LlamaWhisperer
+//
+//  Created by Matt Begnoche on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct LlamaWhispererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
